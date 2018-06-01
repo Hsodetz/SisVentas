79e2fcbb8372bf6c6c4545983f29caa7039188c3
Helmuth Sodetz <sodehl2001@gmail.com>
2018-05-31 19:41:19 -0500

    Adminlte 4 configurada, sweetalert2, tabla roles, campos agregados al registro

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('role_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->string('name');
+            $table->string('username');
+            $table->string('apellidos');
+            $table->string('telefono');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('estado');
+            
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
